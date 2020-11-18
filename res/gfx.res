ae4c2727967aae0aa1b497e58000f4bcfc7b2958
zerasul <zerasul@gmail.com>
2020-11-18 20:48:25 +0100

    updating tileset and initial aproaching

--- res/gfx.res
+++ res/gfx.res
@@ -1,2 +1,4 @@
 IMAGE gameover "gameover.png" BEST
 IMAGE snake_background "snake_background.png" BEST
+TILESET moontlset "moontlset.png" 0
+PALETTE moontlset_pal "moontlset.png" 0
--- src/phase1.c
+++ src/phase1.c
@@ -1,21 +1,35 @@
 #include <genesis.h>
 #include "phase1.h"
 #include "sprt.h"
+#include "gfx.h"
 
 void * phase1_init(){
 
     void * data = MEM_alloc(sizeof(struct phase1_data));
     struct phase1_data* mystruct = (data);
-
+    u8 i,j;
     mystruct->life=100;
     mystruct->lives=3;
     mystruct->player_x=20;
     mystruct->player_y=20;
 
+    VDP_loadTileSet(&moontlset,mystruct->tile_index,DMA);
+    VDP_setPalette(PAL0,moontlset_pal.data);
+        for (i = 0; i < 40; i++)
+    {
+        for (j = 0; j < 28; j++)
+        {
+            VDP_setTileMapXY(BG_A, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, mystruct->tile_index + mymap[i + 40 * j]), i, j);
+        }
+    }
+    mystruct->tile_index+=moontlset.numTile;
+
     VDP_setPalette(PAL1,playersprt.palette->data);
     Sprite* sprt=SPR_addSprite(&playersprt, mystruct->player_x, mystruct->player_y,TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, mystruct->tile_index));
-    SPR_setAnim(sprt, RIGHT);
+    SPR_setAnim(sprt, IDLE);
     mystruct->player_sprt=sprt;
+    
+    
     return data;
 }
 
