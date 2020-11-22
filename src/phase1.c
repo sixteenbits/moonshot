6a9c318dbc16db55a21501631e08ca411653a688
zerasul <zerasul@gmail.com>
2020-11-22 17:16:27 +0100

    ultimos cambios

--- src/phase1.c
+++ src/phase1.c
@@ -12,17 +12,24 @@ void * phase1_init(){
     mystruct->lives=3;
     mystruct->player_x=20;
     mystruct->player_y=20;
+    mystruct->tile_index=TILE_USERINDEX;
 
     VDP_loadTileSet(&moontlset,mystruct->tile_index,DMA);
     VDP_setPalette(PAL0,moontlset_pal.data);
-        for (i = 0; i < 40; i++)
-    {
-        for (j = 0; j < 28; j++)
+
+
+   
+    
+      for (i = 0; i < 40; i++)
         {
+        for (j = 0; j < 28; j++)
+         {
+            
             VDP_setTileMapXY(BG_A, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, mystruct->tile_index + mymap[i + 40 * j]), i, j);
+         }
         }
-    }
-    mystruct->tile_index+=moontlset.numTile;
+
+     mystruct->tile_index+=moontlset.numTile;
 
     VDP_setPalette(PAL1,playersprt.palette->data);
     Sprite* sprt=SPR_addSprite(&playersprt, mystruct->player_x, mystruct->player_y,TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, mystruct->tile_index));
